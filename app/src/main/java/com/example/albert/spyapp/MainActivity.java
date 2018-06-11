ef6fa50fac6498b4ef9881ccaaadbe7a839790de
Albert <albert.kulinski@hotmail.com>
2018-06-11 23:19:45 +0200

    testing connection working

--- app/src/main/java/com/example/albert/spyapp/MainActivity.java
+++ app/src/main/java/com/example/albert/spyapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.albert.spyapp;
 
 import android.app.DownloadManager;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,42 +25,32 @@ public class MainActivity extends AppCompatActivity {
     private ReentrantLock lock;
     private  TextView textView;
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        setContentView(R.layout.activity_main);
+        textView=(TextView) findViewById(R.id.connected);
+
+        final ServerClient serverClient=new ServerClient(this);
+        serverClient.testConnection();
+        if(serverClient.isConnected())
+        {
+            Log.d("Main View","true");
+            textView.setText("connected");
+            textView.setTextColor(Color.parseColor("#00FF00"));
+        }
+        else
+        {
+            Log.d("Main View","not connected");
+            textView.setText("connected");
+            textView.setTextColor(Color.parseColor("#00FF00"));
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ServerClient serverClient=new ServerClient(this);
-
-        serverClient.testConnection();
-
-        textView=(TextView) findViewById(R.id.connected);
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true)
-                {
-                    if(serverClient.isConnected())
-                    {
-                        Log.d("Main View","true");
-                        textView.setText("connected");
-                        textView.setTextColor(Color.parseColor("#00FF00"));
-                    }
-                    else
-                    {
-                        Log.d("Main View","not connected");
-                        textView.setText("connected");
-                        textView.setTextColor(Color.parseColor("#00FF00"));
-                    }
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-        }).run();
-
 
     }
 }
