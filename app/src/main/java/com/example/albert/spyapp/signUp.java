856c5c5b233eecc413a87707e71cf67196192959
Albert <albert.kulinski@hotmail.com>
2018-06-17 10:30:40 +0200

    added hashing for password

--- app/src/main/java/com/example/albert/spyapp/signUp.java
+++ app/src/main/java/com/example/albert/spyapp/signUp.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -17,45 +20,63 @@ public class signUp extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private EditText repassword;
+    private String passwd = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        confirm=(Button) findViewById(R.id.confirmSignUp);
+        confirm = findViewById(R.id.confirmSignUp);
 
-        login=(EditText) findViewById(R.id.username);
-        email=(EditText) findViewById(R.id.enteremail);
-        password=(EditText) findViewById(R.id.password);
-        repassword=(EditText) findViewById(R.id.passwordrenter);
+        login = findViewById(R.id.username);
+        email = findViewById(R.id.enteremail);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.passwordrenter);
 
 
         confirm.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                final ServerPost serverPost=new ServerPost("http://192.168.0.37:4567/stalker/addStalker",v.getContext());
-                serverPost.addToPost("name",login.getText().toString());
-                serverPost.addToPost("id","1");
-                serverPost.addToPost("password",password.getText().toString());
-                serverPost.addToPost("email",email.getText().toString());
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-                serverPost.addToPost("dateOfJoining",timeStamp );
-                serverPost.addToPost("lastOnline", timeStamp);
+                if (checkpasswords()) {
+                    hashPassword();
+                    final ServerPost serverPost = new ServerPost("http://192.168.0.37:4567/stalker/addStalker", v.getContext());
+                    serverPost.addToPost("name", login.getText().toString());
 
-               Thread th = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        serverPost.postStalker();
-                    }
-                });
-                th.run();
-                try {
-                    th.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    //date and id are figured out by server
+                    serverPost.addToPost("id", "1");
+                    serverPost.addToPost("email", email.getText().toString());
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+                    serverPost.addToPost("dateOfJoining", timeStamp);
+                    serverPost.addToPost("lastOnline", timeStamp);
+                    serverPost.addToPost("password", passwd);
+                    Thread th = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverPost.postStalker();
+                        }
+                    });
+                    th.run();
                 }
             }
         });
     }
+
+
+    protected void hashPassword() {
+        passwd = password.getText().toString();
+        passwd = Hashing.sha256().hashString(passwd, StandardCharsets.UTF_8).toString();
+    }
+
+    boolean checkpasswords() {
+        return password.getText().toString().equals(repassword.getText().toString());
+
+    }
+
+
 }
+
+
+
+
