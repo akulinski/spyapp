cd20b73bc7c4ef4e51cd19a512473b6c82a55ae4
Pakulaa <Konrad.Pakula@Live.umcs.edu.pl>
2018-06-20 18:27:37 +0200

    Rebuild Project

--- app/src/main/java/com/example/albert/spyapp/CameraActivity.java
+++ app/src/main/java/com/example/albert/spyapp/CameraActivity.java
@@ -2,11 +2,8 @@ package com.example.albert.spyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
